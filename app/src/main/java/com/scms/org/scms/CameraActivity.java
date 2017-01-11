ded931ac1c3149bfcb485273aa76f479e168947b
Drkjoyy <alexander.aolaritei@gmail.com>
2017-01-10 19:17:01 -0500

    Added a permission and a method for taking pictures

--- app/src/main/java/com/scms/org/scms/CameraActivity.java
+++ app/src/main/java/com/scms/org/scms/CameraActivity.java
@@ -1,7 +1,22 @@
 package com.scms.org.scms;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
 /**
  * Created by Alex on 1/10/2017.
  */
-public class CameraActivity {
+public class CameraActivity extends Activity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+    
 }
