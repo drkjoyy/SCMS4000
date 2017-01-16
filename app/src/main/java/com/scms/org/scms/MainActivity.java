28f3e347876588913f81dec2b4cca79138226e2d
Drkjoyy <alexander.aolaritei@gmail.com>
2017-01-15 23:23:53 -0500

    Camera activity now shows preview of what phone camera sees

--- app/src/main/java/com/scms/org/scms/MainActivity.java
+++ app/src/main/java/com/scms/org/scms/MainActivity.java
@@ -1,13 +1,17 @@
 package com.scms.org.scms;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(getApplicationContext(), CameraActivity.class);
+        MainActivity.this.startActivity(intent);
     }
 }
