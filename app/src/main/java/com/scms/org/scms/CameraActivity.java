40f1dbff78cee625014756bed9b644097c03c180
Drkjoyy <alexander.aolaritei@gmail.com>
2017-01-20 15:59:12 -0500

    The app can now take a picture and there is a test image overlay over the camera preview. This overlay will eventually be replaced with solid colors.

--- app/src/main/java/com/scms/org/scms/CameraActivity.java
+++ app/src/main/java/com/scms/org/scms/CameraActivity.java
@@ -1,17 +1,25 @@
 package com.scms.org.scms;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -19,6 +27,16 @@ import android.widget.ImageView;
 public class CameraActivity extends Activity {
     private Camera mCamera = null;
     private CameraView mCameraView = null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static Bitmap imgResult = null;
+
+    public Bitmap screenShot(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(),
+                view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,5 +62,61 @@ public class CameraActivity extends Activity {
                 System.exit(0);
             }
         });
+        //ImageView img = (ImageView) findViewById(R.id.imgResult);
+        //img.setImageResource(R.drawable.demo_level_planet);
+        //img.setImageBitmap(R.drawable.demo_level_planet);
+        //img.getLayoutParams().height = 200;
+        //img.getLayoutParams().width = 200;
+        //btn to capture photo
+        Button imgCapture = (Button)findViewById(R.id.capture);
+        imgCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgResult = screenShot(mCameraView);
+                mCamera.takePicture(null, null, mPicture);
+                System.out.println("TOOK THE IMAGE");
+                //Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                //CameraActivity.this.startActivity(intent);
+            }
+        });
+    }
+
+    Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                return;
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+
+            } catch (IOException e) {
+            }
+        }
+    };
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
     }
 }
