d7f310292e14eb41844806a15362a3058d60d244
Drkjoyy <alexander.aolaritei@gmail.com>
2017-01-26 00:23:31 -0500

    App will flash 3 colors, take 3 pics, and save these 3 pics to gallery

--- app/src/main/java/com/scms/org/scms/CameraActivity.java
+++ app/src/main/java/com/scms/org/scms/CameraActivity.java
@@ -15,6 +15,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
@@ -33,6 +34,9 @@ public class CameraActivity extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static Bitmap imgResult = null;
     static final String MEDIA_FILE = "mediafile";
+    ImageView redReference;
+    ImageView greenReference;
+    ImageView blueReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,17 +78,24 @@ public class CameraActivity extends Activity {
         imgCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCamera.takePicture(null, null, mPicture);
-                System.out.println("TOOK THE IMAGE");
-                finish();
+                runThread();
             }
         });
+
+        redReference = (ImageView)findViewById(R.id.redReference);
+        redReference.setAlpha(0);
+        greenReference = (ImageView)findViewById(R.id.greenReference);
+        greenReference.setAlpha(0);
+        blueReference = (ImageView)findViewById(R.id.blueReference);
+        blueReference.setAlpha(0);
+
     }
 
     private final Camera.PictureCallback mPicture = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
-            File pictureFile = getOutputMediaFile();
+            galleryAddPic();
+            File pictureFile = getOutputMediaFile(1);
             if (pictureFile == null) {
                 setResult(RESULT_CANCELED);
                 return;
@@ -97,27 +108,99 @@ public class CameraActivity extends Activity {
                 returnedData.putExtra(MEDIA_FILE, pictureFile);
                 setResult(RESULT_OK, returnedData);
             } catch (FileNotFoundException e) {
-
             } catch (IOException e) {
             }
         }
     };
 
-    private static File getOutputMediaFile() {
-        File mediaStorageDir = new File(
-                Environment
-                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                "MyCameraApp");
-        if (!mediaStorageDir.mkdirs()) { // mkdirs() checks for exists() first.
-            Log.d(TAG, "failed to create directory");
-            return null;
+    private void runThread() {
+        Thread s = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                redReference.setAlpha(255);
+                                mCamera.takePicture(null, null, mPicture);
+                                mCamera.startPreview();
+                            }
+                        });
+                        wait(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                redReference.setAlpha(0);
+                                greenReference.setAlpha(255);
+                                mCamera.takePicture(null, null, mPicture);
+                                mCamera.startPreview();
+                            }
+                        });
+                        wait(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                greenReference.setAlpha(0);
+                                blueReference.setAlpha(255);
+                                mCamera.takePicture(null, null, mPicture);
+                                mCamera.startPreview();
+                            }
+                        });
+                        wait(1000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                blueReference.setAlpha(0);
+                            }
+                        });
+                        finish();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+        };
+        s.start();
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = getOutputMediaFile(1);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
         }
 
         // Create a media file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
-                .format(new Date());
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         File mediaFile;
-        mediaFile = new File(mediaStorageDir, "IMG_" + timeStamp + ".jpg");
+        if (type == 1){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == 2) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
 
         return mediaFile;
     }
